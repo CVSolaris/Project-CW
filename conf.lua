c1135b4cf7e76385ab6cb508bf0563ef6afac9cb
CVSolaris <cvsarmient13@gmail.com>
2023-03-10 00:43:34 +0000

    Update

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.title = "game"
+    t.version = "11.3"
+    t.console = false
+    t.window.width = 1920
+    t.window.height = 1080
+end
---
+++ main.lua
@@ -0,0 +1,16 @@
+require("player")
+
+function love.load()
+    world = wf.newWorld(0, 0, false)
+    Player:load()
+end
+
+function love.update(dt)
+    world:update(dt)
+    Player:update()
+end
+
+function love.draw()
+    Player:draw()
+    world:draw() --NICE ONE
+end
